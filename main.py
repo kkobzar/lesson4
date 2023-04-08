f4ed28bf3dd75bbbbf34d74bc46fa7c81932c4ae
kkobzar <kirilkobzar@gmail.com>
2023-04-08 11:37:19 +0300

    added class Teacher

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ class Student(Person):
     def __init__(self,name):
         super().__init__(name, 15)
 
+class Teacher(Person):
+
+    def __init__(self, name):
+        super().__init__(name, 25)
+
+    def give_mark(self, student):
+        print('Teacher gave grade to', student.name)
+        student.mark = 12
 
 
 good_student1 = Student("Sasha")
